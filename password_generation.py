2c15eb580e702efa1209db096ce369becc4a9eaa
heyhenry <henryyy3@hotmail.com>
2025-03-07 22:23:51 +0800

    Created password generation algorithm

---
+++ password_generation.py
@@ -0,0 +1,23 @@
+import random
+import string
+
+# goal of password the length of 16 mixed 4 puncs, 8 string, 4 digits
+
+# get password content
+a_string = string.ascii_letters
+a_nums = string.digits
+a_puncs = string.punctuation
+password = ""
+
+# get mixed characters from each content category (string, digits, punctuation)
+password += "".join(random.choices(a_string, k=8))
+password += "".join(random.choices(a_nums, k=4))
+password += "".join(random.choices(a_puncs, k=4))
+
+# shuffle the password 
+password = list(password)
+random.shuffle(password)
+password = "".join(password)
+
+# display newly generated password
+print(password)
