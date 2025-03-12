e08b213d5157250b4a588d3c430833ac9a53d2a0
heyhenry <henryyy3@hotmail.com>
2025-03-12 14:58:07 +0800

    Updated main.py file with updated structure.

--- main.py
+++ main.py
@@ -1,49 +1,21 @@
 import argon2
 import sqlite3
+from authmanager import AuthManager
 
-# currently stored hash is for the string literal pw: Peekaboo (verificaiton is case sensitive!)
+# create the auth object instance
+auth = AuthManager()
 
-connection = sqlite3.connect("db/auth.db")
-cursor = connection.cursor()
+# welcome message
+print("[[[ Welcome to Valutic ]]]")
 
-ph = argon2.PasswordHasher()
-
-# check database to see if a master password has been set
-get_password_query = "SELECT hash FROM authentication"
-cursor.execute(get_password_query)
-result = cursor.fetchone()
-password_found = False
-if result[0] != '':
-    password_found = True
-
-if password_found:
-    input_password = input("Enter your master password: ")
-
-    # retrieve the master password's hash
-    get_hashed_master_password = "SELECT hash FROM authentication WHERE rowid = 1"
-    cursor.execute(get_hashed_master_password)
-    result = cursor.fetchone()
-    result_master_password = result[0]
-    # print(result_master_password)
-
-    # verify password against stored hash to see if password is correct
-    try:
-        ph.verify(result_master_password, input_password)
-        print("CONGRATLATIONS!! YOU'VE ENTERED YOUR VAULT SUCCESSFULLY!!!!!")
-    except argon2.exceptions.VerifyMismatchError:
-        print("Your input password does not match the given master password!")
+# if a valid hash is found in the auth.db, proceed to ask for the password and verify
+if auth.get_stored_hash():
+    input_password = input("Enter your Master Password: ")
+    auth.verify_master_password(input_password)
+# if a valid hash is not found in the auth.db, proceed to ask user for a new master password and store in auth.db
 else:
-    # prompt user to enter a new master password
-    master_password = input("Enter new master password: ")
-    print(f"master password: {master_password}")
+    master_password = input("Enter a New Master Password: ")
+    auth.set_master_password(master_password)
 
-    # hash the new master password
-    hashed_master_password = ph.hash(master_password)
-    print(f"hashed master password: {hashed_master_password}")
-    
-    # insert the new master password into the authentication database
-    insert_new_password = "UPDATE authentication SET hash = ? WHERE rowid = 1" # safer than f"" which is susceptible to sql injection attacks
-    cursor.execute(insert_new_password, (hashed_master_password,))
-    connection.commit()
-
-connection.close()
+# close the database after usage
+auth.close_database()
