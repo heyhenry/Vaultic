dcfa7c4be400d9933d1ad8c7bb16bff91ad787c4
heyhenry <henryyy3@hotmail.com>
2025-03-12 13:50:41 +0800

    create master password hash and update auth.db with user input

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import sqlite3
 connection = sqlite3.connect("db/auth.db")
 cursor = connection.cursor()
 
-# first git commit 
 # check database to see if a master password has been set
 get_password_query = "SELECT hash FROM authentication"
 cursor.execute(get_password_query)
@@ -12,5 +11,22 @@ result = cursor.fetchone()
 password_found = False
 if result[0] != '':
     password_found = True
+
+if password_found:
+    print("Enter your master password: ")
+else:
+    # prompt user to enter a new master password
+    master_password = input("Enter new master password: ")
+    print(f"master password: {master_password}")
+
+    # hash the new master password
+    ph = argon2.PasswordHasher()
+    hashed_master_password = ph.hash(master_password)
+    print(f"hashed master password: {hashed_master_password}")
+    
+    # insert the new master password into the authentication database
+    insert_new_password = "UPDATE authentication SET hash = ? WHERE rowid = 1"
+    cursor.execute(insert_new_password, (hashed_master_password,))
+    connection.commit()
 
 connection.close()
