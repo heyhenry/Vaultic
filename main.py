9cf24a616b615fd82153cbbf2c202477b8f36171
heyhenry <henryyy3@hotmail.com>
2025-03-12 13:55:54 +0800

    Get hashed master password from auth.db

--- main.py
+++ main.py
@@ -13,7 +13,13 @@ if result[0] != '':
     password_found = True
 
 if password_found:
-    print("Enter your master password: ")
+    input_password = input("Enter your master password: ")
+
+    get_hashed_master_password = "SELECT hash FROM authentication WHERE rowid = 1"
+    cursor.execute(get_hashed_master_password)
+    result = cursor.fetchone()
+    result_master_password = result[0]
+    print(result_master_password)
 else:
     # prompt user to enter a new master password
     master_password = input("Enter new master password: ")
@@ -23,9 +29,9 @@ else:
     ph = argon2.PasswordHasher()
     hashed_master_password = ph.hash(master_password)
     print(f"hashed master password: {hashed_master_password}")
-    
+
     # insert the new master password into the authentication database
-    insert_new_password = "UPDATE authentication SET hash = ? WHERE rowid = 1"
+    insert_new_password = "UPDATE authentication SET hash = ? WHERE rowid = 1" # safer than f"" which is susceptible to sql injection attacks
     cursor.execute(insert_new_password, (hashed_master_password,))
     connection.commit()
 
