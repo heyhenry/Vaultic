a9d5c6326afd98ef43546bcb4ce9438faa985c0d
heyhenry <henryyy3@hotmail.com>
2025-03-12 13:11:31 +0800

    Added logic comments to create_auth_database file

--- create_auth_database.py
+++ create_auth_database.py
@@ -1,20 +1,23 @@
 import sqlite3
 
+# open auth.db else create auth.db if it doesn't exist
 connection = sqlite3.connect("db/auth.db")
 cursor = connection.cursor()
 
+# create the auth table
 create_table_statement = '''
 CREATE TABLE IF NOT EXISTS authentication (
     desc TEXT,
     hash TEXT
 );
 '''
-
+# execute above statement
 cursor.execute(create_table_statement)
 
 # insert default info for desc with an empty value for hash upon initial creation
 default_details_statement = "INSERT INTO authentication (desc, hash) values ('master_password', '')"
 
+# execute above statement
 cursor.execute(default_details_statement)
 
 connection.commit()
