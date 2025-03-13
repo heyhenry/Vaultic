063ec804f81d8aa2b85c11c64d4d446dc663756e
heyhenry <henryyy3@hotmail.com>
2025-03-13 19:59:59 +0800

    Added a new column: salt

--- create_auth_database.py
+++ create_auth_database.py
@@ -8,14 +8,15 @@ cursor = connection.cursor()
 create_table_statement = '''
 CREATE TABLE IF NOT EXISTS authentication (
     desc TEXT,
-    hash TEXT
+    hash TEXT,
+    salt TEXT
 );
 '''
 # execute above statement
 cursor.execute(create_table_statement)
 
 # insert default info for desc with an empty value for hash upon initial creation
-default_details_statement = "INSERT INTO authentication (desc, hash) values ('master_password', '')"
+default_details_statement = "INSERT INTO authentication (desc, hash, salt) values ('master_password', '', '')"
 
 # execute above statement
 cursor.execute(default_details_statement)
