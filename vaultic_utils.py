86cfb67dafe44bc23dd7333386c2c41bf913e6e1
heyhenry <henryyy3@hotmail.com>
2025-04-07 15:05:09 +0800

    Created a tooltip function.

--- vaultic_utils.py
+++ vaultic_utils.py
@@ -1,4 +1,7 @@
+from ttkbootstrap.constants import *
 from ttkbootstrap.toast import ToastNotification
+from ttkbootstrap.tooltip import ToolTip
+
 
 def show_toast(title, message, duration):
     ToastNotification(
@@ -6,3 +9,9 @@ def show_toast(title, message, duration):
         message=message,
         duration=duration
     ).show_toast()
+
+def show_tooltip(widget, text):
+    ToolTip(widget=widget, text=text, bootstyle=INFO).show_tip()
+
+def print_out(event=None):
+    print("POOP!!")
